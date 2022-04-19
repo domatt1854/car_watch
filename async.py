58f7f34a41da6bb40c949ae8368086750c53f91f
Matthew Do <domatthew1854@gmail.com>
2022-04-19 10:06:54 -0700

    working on async

--- async.py
+++ async.py
@@ -1,19 +1,67 @@
-import aiohttp
 import asyncio
 from bs4 import BeautifulSoup
+import requests
 
 
-async def get_proxies(session):
-    url = 'https://free-proxy-list.net/'
-    async with session.get(url) as r:
-        return await process_proxies(r.text())
+class CarScraper:
 
+    def __init__(self):
+        self.ua = []
+        self.proxies = []
 
-def process_proxies(response):
+    async def get_user_agents(self):
+
+        with open('data/useragents.txt', 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            self.ua.append(line.strip())
+        
+        
+
+    async def get_proxies(self):
     
-    soup = BeautifulSoup(
-        response,
-        'html.parser'
+        proxy_website = 'https://free-proxy-list.net/'
+
+        response = requests.get(proxy_website)
+        proxies = []
+
+        soup = BeautifulSoup(
+            response.content,
+            'html.parser'
+        )       
+
+        for row in soup.find("table").find_all('tr'):
+            if(len(row.find_all('td')) > 0):
+                ip = str(row.find_all('td')[0]).strip('<td>').strip('</')
+                port = str(row.find_all('td')[1]).strip('<td>').strip('</')
+                self.proxies.append(ip + ":" + port)
+
+                print("{}:{}".format(ip, port))
+            
+    
+    # todo: hit the website with every make and calculate how much pages I should parse
+    async def check_num_results(make,)
+        
+
+
+
+
+async def main():
+    cs = CarScraper()
+
+    get_proxies_task = asyncio.create_task(
+        cs.get_proxies()
     )
+    
+    get_ua_task = asyncio.create_task(
+        cs.get_user_agents()
+    )
+
+    await get_proxies_task
+    await get_ua_task
+
+
+
 
     
