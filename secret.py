94857e4f30fbe4f899c863a2dd3a243f9bebae36
manylayers <domatthew1854@gmail.com>
2022-05-12 03:10:15 -0700

    Added secret.py

---
+++ secret.py
@@ -0,0 +1,7 @@
+from os import environ
+
+PORT = environ.get("PORT")
+PASSWORD = environ.get("PASSWORD")
+USERNAME = environ.get("USERNAME")
+DATABASE = environ.get("DB")
+ENDPOINT = environ.get("HOST")
